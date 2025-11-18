55058f2307d1ac2c6f6448aeb7fa07d0f5f9d3b9
wb0999 <wb0999@live.cn>
2025-11-18 15:30:59 +0800

    Update routes to use wb0999 instead of CUSTOM_DOMAIN

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  ["docker." + CUSTOM_DOMAIN]: dockerHub,
-  ["quay." + CUSTOM_DOMAIN]: "https://quay.io",
-  ["gcr." + CUSTOM_DOMAIN]: "https://gcr.io",
-  ["k8s-gcr." + CUSTOM_DOMAIN]: "https://k8s.gcr.io",
-  ["k8s." + CUSTOM_DOMAIN]: "https://registry.k8s.io",
-  ["ghcr." + CUSTOM_DOMAIN]: "https://ghcr.io",
-  ["cloudsmith." + CUSTOM_DOMAIN]: "https://docker.cloudsmith.io",
-  ["ecr." + CUSTOM_DOMAIN]: "https://public.ecr.aws",
+  ["docker." + wb0999]: dockerHub,
+  ["quay." + wb0999]: "https://quay.io",
+  ["gcr." + wb0999]: "https://gcr.io",
+  ["k8s-gcr." + wb0999]: "https://k8s.gcr.io",
+  ["k8s." + wb0999]: "https://registry.k8s.io",
+  ["ghcr." + wb0999]: "https://ghcr.io",
+  ["cloudsmith." + wb0999]: "https://docker.cloudsmith.io",
+  ["ecr." + wb0999]: "https://public.ecr.aws",
 
   // staging
-  ["docker-staging." + CUSTOM_DOMAIN]: dockerHub,
+  ["docker-staging." + wb0999]: dockerHub,
 };
 
 function routeByHosts(host) {
